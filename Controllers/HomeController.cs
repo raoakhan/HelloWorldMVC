29524760df1d4801ba38084520fc73b513c8c9c4
akmal_shahbaz@yahoo.com <akmal_shahbaz@yahoo.com>
2021-07-22 12:39:49 +0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using HelloWorldMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorldMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            AppMessage obj = new AppMessage()
+            {
+                Message = "Hello World!"
+            };
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Index(AppMessage obj)
+        {
+            ViewBag.Message = "Message changed.";
+            return View(obj);
+        }
+
+    }
+}
